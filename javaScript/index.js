cf9953de50a8485b3982d4a7be9a5b5903b891a4
LahiruMudith <lahimudith@gmail.com>
2023-11-10 01:13:29 +0530

    java Script

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,1 +1,30 @@
 console.log("Lahiru_Mudith");
+
+//Alert
+// alert("Lahiru");
+
+document.write("Kalapugama" + "\n");
+document.writeln("Moronthuduwa" + "<br>");
+document.write("<h1>" +"Panadura"+  "</h1>");
+
+// Global variable
+var name = "Lahiru";
+
+// Local variable
+{
+    var town = "Panadura";
+}
+
+
+
+document.write( "<h3>"+"Lahiru" + "\n");
+document.writeln("Mudith" + "\n" );
+document.writeln("Madushan" + "\n"+ "<br>"  );
+document.write("age = 17" + "<br>" );
+document.write("Brithday : 2006.05.24" + "<br>" );
+document.write("Panadura"+ "<h3>" + "<br>" );
+
+
+document.write(
+    "<h3>"+ "Name: Lahiru" +"\n" + "Mudith"  +"\n"+ "Madushan" +"<br>"+ "age: 17" +"<br>"+ "Brithday: 2006.05.24" +"<br>"+ "Town: Panaduara"
+      );
